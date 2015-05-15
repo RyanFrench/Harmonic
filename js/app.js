f2791c3648b454647c4b88927e5348527dd2e836
Ryan French <ryan@ryanfrench.co>
2015-05-15 18:34:06 +1200

    Updated for new material design version of google play music

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ var ipc = require('ipc');
 
 ipc.on('ping', function(arg){
   var webView = document.querySelector('webview#gpm-player');
-  webView.executeJavaScript("document.querySelector('button[data-id=\"" + arg + "\"]').click()");
+  webView.executeJavaScript("document.querySelector('sj-icon-button[data-id=\"" + arg + "\"]').click()");
 });
